cab397868c95a85d5ee4ae4cf7af00973c3765d2
Sam Denty <sam@samdenty.com>
2020-12-13 22:41:18 +0000

    add setup command

--- brew.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-brew install gnupg
-brew link --overwrite gnupg
-brew install pinentry-mac
-echo "pinentry-program /usr/local/bin/pinentry-mac" >>~/.gnupg/gpg-agent.conf
-killall gpg-agent
--- setup-a-new-machine.zsh
+++
@@ -1,4 +0,0 @@
-# Download and install zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-source macos.zsh
